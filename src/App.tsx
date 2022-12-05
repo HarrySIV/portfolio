563dbc37ecb7f4e747b13d96102fc54e635da4c8
Harry Sanders <harry.sandersiv@yahoo.com>
2022-12-05 13:11:40 -0500

    frontend portfolio init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Edit app and save to reload.</p>
+      </header>
+    </div>
+  );
+}
