e0d89c43cd2b2ddf9954a2ee7f90bda1bf45b527
Harry Sanders <harry.sandersiv@yahoo.com>
2022-12-13 18:45:54 -0500

    added font imports and buttons for projects

--- src/pages/homepage/projects/ProjectsCard.tsx
+++ src/pages/homepage/projects/ProjectsCard.tsx
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ProjectsHook } from '../../../hooks/ProjectsHook';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faSquareCaretLeft,
+  faSquareCaretRight,
+} from '@fortawesome/free-solid-svg-icons';
+
 export function ProjectsCard() {
+  const [showAlt, setShowAlt] = useState(false);
   const { projects } = ProjectsHook();
+
+  const clickHandler = () => {
+    setShowAlt(!showAlt);
+  };
   return (
     <>
       {projects.map((project) => (
@@ -14,12 +25,27 @@ export function ProjectsCard() {
           }}
           href={project.link}
         >
-          <div className="h-16 p-4 pt-12 z-10 bg-opacity-0 absolute">
-            <h2 className="m-0 p-4 text-6xl text-black bg-white/80">
+          <div className="z-10 bg-opacity-0 flex items-center">
+            {!showAlt ? (
+              <button
+                className="z-20 border-black bg-white"
+                onClick={clickHandler}
+              >
+                <FontAwesomeIcon icon={faSquareCaretLeft} />
+              </button>
+            ) : null}
+            <h2 className="m-0 p-4 text-[6vw] md:text-6xl text-black bg-white/80">
               {project.name}
             </h2>
+            {!showAlt ? (
+              <button
+                className="z-20 border-black bg-white"
+                onClick={clickHandler}
+              >
+                <FontAwesomeIcon icon={faSquareCaretRight} />
+              </button>
+            ) : null}
           </div>
-          <hr />
           <h6 id={`${project.id}`}>{project.description}</h6>
           <h5>{project.tools}</h5>
         </a>
